c08992cc6d6b846aa7c7766485b89fc2deb98f48
Reyhan Quayum <rrq2003@nyu.edu>
2023-12-16 13:19:07 -0500

    planning

---
+++ main.py
@@ -0,0 +1,8 @@
+hanafi = False # madhab the user selects -- will change Asr and how prayer is combined
+
+'''okay, need to find a good API to use, so far have looked at
+1. https://rapidapi.com/oag-oag-default/api/flight-info-api/pricing - freemium (50 requets / month)
+2. flightAware API - has same problem as above
+3. automating google search and web scraping that -- goes against terms of service LOL
+4. https://aviationstack.com/ -- need to investigate this, looks promising (from https://github.com/public-apis/public-apis#transportation)
+'''`
